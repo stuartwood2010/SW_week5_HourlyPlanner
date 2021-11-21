8ab6f3b81e43ff77637b276d7d8131964b714e38
Stuart Wood <stuartwood2010@hotmail.com>
2021-11-20 23:23:25 -0700

    Everything working except local storage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let jumbotronEl = $('.jumbotron');
 let currentDayEl = $('#currentDay');
 let containerEl = $('.container');
 containerEl.addClass('col row justify-content-center');
-
+let contentEl = $('<input>');
 /*Using Moment.js, we grab the current date and put it on the jumbotron,
 we set current time to the current hour only */
 currentDayEl.text(moment().format('dddd MMMM DD, YYYY'));
@@ -24,31 +24,35 @@ const weeklyPlanEl = [
 For every object in the weeklyPlanEl array, we create a row with the hour, 
 a text area, and a save button.*/
 for (let i = 0; i < weeklyPlanEl.length; i++) {
+    let meetings = localStorage.getItem('events');
     let hourEl = $('<li>');
     hourEl.text(weeklyPlanEl[i].text);
     hourEl.attr('value', i);
     hourEl.addClass('hour row col-1');
     hourEl.css({'list-style-type': 'none', 'font-weight': 'bold'});
-    let contentEl = $('<textarea>');
+    let contentEl = $('<input>');
+    contentEl.attr('type', 'text');
     contentEl.addClass('time-block row description col-10');
     let saveEl = $('<button>Save</button>');
     saveEl.addClass('saveBtn col-1');
     containerEl.append(hourEl);
     containerEl.append(contentEl);
     containerEl.append(saveEl);
-    contentEl.css({'border-top': '1px solid black'})
-    /*We style the planner based on whether an hour is in the past, present, or future
-    We do so using a 24 hr clock and comparing it to the value of the object in the
-    current weeklyPlanEl array */
-    if (currentHour < weeklyPlanEl[i].value) {
+    contentEl.css({'border-top': '1px solid black'});
+    
+/*We style the planner based on whether an hour is in the past, present, or future
+We do so using a 24 hr clock and comparing it to the value of the object in the
+current weeklyPlanEl array */
+if (currentHour < weeklyPlanEl[i].value) {
         contentEl.addClass('future');
     } else if (currentHour > weeklyPlanEl[i].value) {
         contentEl.addClass('past');
     } else {
         contentEl.addClass('present');
     }
-    saveEl.on('click', saveEvent());
-};
-function saveEvent() {
-    
+    saveEl.on('click', function saveEvent(event) {
+        event.preventDefault();        
+        localStorage.setItem('events', meetings);
+});
 }
+
